02b9a191b3a3fb48907262e265dbf061a0992b32
AresHidalgo <foreveryalone88@gmail.com>
2025-06-26 01:15:53 -0500

    section-3 settings

---
+++ src/layouts/shared/data.js
@@ -0,0 +1,22 @@
+// src/layouts/shared/data.js
+import { FileText, HomeIcon, ShieldCheck } from "lucide-react";
+
+const features = [
+    {
+        icon: FileText,
+        title: "Amplia gama de propiedades",
+        desc: "Ofrecemos asistencia legal especializada para todo lo relacionado con la propiedad en Colombia.",
+    },
+    {
+        icon: HomeIcon,
+        title: "Comprar o alquilar casas",
+        desc: "Vendemos tu casa al mejor precio del mercado y además con mucha rapidez.",
+    },
+    {
+        icon: ShieldCheck,
+        title: "Con la confianza de miles de personas",
+        desc: "Te ofrecemos asesoría gratuita para obtener un préstamo para tu nueva vivienda.",
+    }
+];
+
+export default features;

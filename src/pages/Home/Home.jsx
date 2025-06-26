6545d287ee3636616bff618c768968fa2b315d60
juanpablopos <juan_posada82211@elpoli.edu.co>
2025-06-26 02:39:30 -0500

    Avence nro2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
-import Hero from "./components/hero";
-import Section3 from "./components/section-3";
+import Hero from "./components/hero"
+import Section3 from "./components/section-3"
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-red-500">
-      <Hero />
-      <Section3 />
-    </div>
+     <>
+      <Hero/>
+      <Section3/>
+     </>
   );
 }
 

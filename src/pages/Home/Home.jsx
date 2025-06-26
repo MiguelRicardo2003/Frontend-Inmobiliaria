298e60f1799d49b6fe5b8876a2673c9dd3c6f707
AresHidalgo <foreveryalone88@gmail.com>
2025-06-26 05:00:42 -0500

    Component section-4

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/hero";
 import Section3 from "./components/section-3";
+import Section4 from "./components/section-4";
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-red-500">
       <Hero />
       <Section3 />
+      <Section4 />
     </div>
   );
 }

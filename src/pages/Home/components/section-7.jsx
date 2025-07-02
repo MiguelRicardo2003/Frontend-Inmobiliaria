ecc6e081fa52253dcfb1b51c644da25cd6a35560
AresHidalgo <foreveryalone88@gmail.com>
2025-07-02 01:53:24 -0500

    ajustes en secciones y componentes

--- src/pages/Home/components/section-7.jsx
+++ src/pages/Home/components/section-7.jsx
@@ -1,5 +1,5 @@
 import Button from "../../../shared/button";
-import { Phone } from "lucide-react";
+import { Phone, ArrowRight } from "lucide-react";
 const Section7 = () => {
   return (
     <div className="w-full flex flex-col  py-16 px-6">
@@ -22,16 +22,21 @@ const Section7 = () => {
 
         {/* Botones */}
         <div className="h-12 w-full items-center   ">
-          <Button info={"Registrarme Ahora"} className="self-start bg-[#E7C873] hover:bg-yellow-300 text-black font-semibold px-6 py-3 rounded-xl" onClick={() => console.log("hola")} />
+
+          <Button
+            info={"Registrarme Ahora"}
+            className="self-start flex items-center gap-2 bg-[#E7C873] hover:bg-yellow-300 text-black font-semibold px-6 py-3 rounded-xl"
+            icon={<ArrowRight strokeWidth={1.25} />}
+          />
 
           <div className="flex flex-row font-semibold text-black justify-end ">
-            <div className="h-12 w-40 flex  justify-center py-3 gap-5"><Phone strokeWidth={1.25} />+57 123456789</div>  
+            <div className="h-12 w-40 flex  justify-center py-3 gap-5"><Phone strokeWidth={1.25} />+57 123456789</div>
           </div>
         </div>
 
-        
-        
-         
+
+
+
       </div>
     </div>
   );

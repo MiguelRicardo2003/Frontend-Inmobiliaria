ee0d64daf814ca2f6d446c1449ed59cc99282e1d
juanpablopos <juan_posada82211@elpoli.edu.co>
2025-07-01 20:36:29 -0500

    Avance en los testimonios

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/hero"
 import Section3 from "./components/section-3"
 import Section4 from "./components/section-4"
 import Categories from "./components/categories"
+import Section7 from "./components/section-7"
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Categories />
       <Section3 />
       <Section4 />
+      <Section7 />
     </>
   );
 }

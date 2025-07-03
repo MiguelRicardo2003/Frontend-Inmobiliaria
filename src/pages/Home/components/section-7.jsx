dd71235e8c584954121bc9900171021c35749501
juanpablopos <juan_posada82211@elpoli.edu.co>
2025-07-02 22:48:41 -0500

    Avance #4 en la seccion

--- src/pages/Home/components/section-7.jsx
+++ src/pages/Home/components/section-7.jsx
@@ -11,26 +11,33 @@ const Section7 = () => {
         </div>
 
         {/* Subtítulo */}
-        <div className="text-gray-600 sm:text-lg sm:text-right  ">
-          <div >Quieres poner en arriendo tu casa o estas buscando tu hogar ideal,
-            <div className=" flex  px-10">que esperar para contactarnos.</div>
+        <div className="text-gray-700 sm:text-lg sm:text-right mb-11 pr-3">
+          <div>Quieres poner en arriendo tu casa o estas buscando tu hogar ideal,
+            <div className=" flex  px-10">que esperas para contactarnos?.</div>
           </div>
         </div>
 
         {/* Imagenes */}
-
+        <div className=" flex flex-row px-30 justify-center mb-20">
+            <img
+              src="/img/professionals.png"
+              alt="Professionals"
+              className="w-[90%] h-[90%]"
+            />
+        </div>
 
         {/* Botones */}
-        <div className="h-12 w-full items-center   ">
-          <Button info={"Registrarme Ahora"} className="self-start bg-[#E7C873] hover:bg-yellow-300 text-black font-semibold px-6 py-3 rounded-xl" onClick={() => console.log("hola")} />
+        <div className="h-12 w-full items-center">
+          <div className="flex  px-8">
+            <Button info={"Registrarme Ahora"} className="  self-start bg-[#E7C873] hover:bg-yellow-300 text-black font-semibold px-4 py-3 rounded-xl" onClick={() => console.log("hola")} />
+          </div>
+          
 
-          <div className="flex flex-row font-semibold text-black justify-end ">
+          <div className="flex flex-row font-semibold text-black justify-end pr-3">
             <div className="h-12 w-40 flex  justify-center py-3 gap-5"><Phone strokeWidth={1.25} />+57 123456789</div>  
           </div>
         </div>
 
-        
-        
          
       </div>
     </div>

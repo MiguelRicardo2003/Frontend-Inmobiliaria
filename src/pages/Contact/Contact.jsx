e992cea4efcca1ff45a74d290edfcacdea7de89a
MiguelRicardo2003 <miguelricardomm27@gmail.com>
2025-07-05 01:17:14 -0500

    avances en la seccion de contacto

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,42 @@
+import { Phone, MapPin, Mail } from "lucide-react";
+import Form from "./components/Form";
+import Map from "./components/Map";
+
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="px-6 py-10 max-w-7xl mx-auto">
+      {/* Encabezado */}
+      <div className="text-center max-w-2xl mx-auto mb-10">
+        <h1 className="font-bold text-3xl text-gray-900">Contacto</h1>
+        <p className="text-gray-700 mt-6 text-1xl">
+          En Inmobiliaria, estamos listos para ayudarte a encontrar la propiedad ideal para ti. Si tienes preguntas o necesitas más información, no dudes en ponerte en contacto con nosotros.
+        </p>
+      </div>
+
+      {/* Contenido principal */}
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 items-start">
+        <div className="flex flex-col gap-6">
+          <Form />
+
+          <div className="bg-white shadow-md rounded-lg p-6 flex flex-col gap-3 h-full">
+            <div className="flex items-center gap-2 text-gray-800">
+              <Phone className="w-4 h-4" />
+              (+57) 300 254 34 10
+            </div>
+            <div className="flex items-center gap-2 text-gray-800">
+              <MapPin className="w-4 h-4" />
+              Carepa, Colombia
+            </div>
+            <div className="flex items-center gap-2 text-gray-800">
+              <Mail className="w-4 h-4" />
+              inmobiliaria@example.com
+            </div>
+          </div>
+        </div>
+        <Map/>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
---
+++ src/pages/Contact/components/Form.jsx
@@ -0,0 +1,68 @@
+    const Form = () => {
+    return (
+        <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-6">
+        <form className="flex flex-col gap-4">
+            <input
+            type="text"
+            placeholder="Nombre Completo"
+            className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-500"
+            />
+            <input
+            type="email"
+            placeholder="Email"
+            className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-500"
+            />
+            <input
+            type="email"
+            placeholder="Telefóno"
+            className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-500"
+            />
+            <div>
+            <p className="font-bold text-gray-800 mb-2">Me interesa:</p>
+            <div className="flex flex-row gap-2">
+                <label className="inline-flex items-center gap-2 text-gray-700">
+                <input
+                    type="checkbox"
+                    name="interes"
+                    value="comprar"
+                    className="accent-[#2D3A4E] rounded-full"
+                />
+                Comprar
+                </label>
+                <label className="inline-flex items-center gap-2 text-gray-700">
+                <input
+                    type="checkbox"
+                    name="interes"
+                    value="rentar"
+                    className="accent-[#2D3A4E] rounded-full"
+                />
+                Rentar
+                </label>
+                <label className="inline-flex items-center gap-2 text-gray-700">
+                <input
+                    type="checkbox"
+                    name="interes"
+                    value="otro"
+                    className="accent-[#2D3A4E]"
+                />
+                Otro
+                </label>
+            </div>
+            </div>
+            <textarea
+            placeholder="¿En que podemos Ayudarte?"
+            rows="4"
+            className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-500"
+            ></textarea>
+            <button
+            type="submit"
+            className="bg-[#2D3A4E] hover:bg-gray-700 text-white font-semibold py-2 rounded-lg transition"
+            >
+            Enviar mensaje
+            </button>
+        </form>
+        </div>
+    );
+    };
+
+    export default Form;

eae51f73d54ee32a2488ee673b0cf87b99ec3c93
AresHidalgo <foreveryalone88@gmail.com>
2025-07-08 01:07:59 -0500

    Secc:Propiedades Exclusivas

--- src/shared/featureOfPropieties.jsx
+++ src/shared/featureOfPropieties.jsx
@@ -1,9 +1,9 @@
 
-const Feature = () => {
+const Feature = ({ type, feature }) => {
     return (
         <div className="absolute left-4 top-2 lg:top-6 flex gap-2">
-            <span className="bg-[#1F4B43] text-white text-xs font-semibold px-3 py-1 rounded-full">EN VENTA</span>
-            <span className="bg-[#E7C873] text-black text-xs font-semibold px-3 py-1 rounded-full">CASA</span>
+            <span className="bg-[#1F4B43] text-white text-xs font-semibold px-3 py-1 rounded-full">{type}</span>
+            <span className="bg-[#E7C873] text-black text-xs font-semibold px-3 py-1 rounded-full">{feature}</span>
         </div>
     );
 }

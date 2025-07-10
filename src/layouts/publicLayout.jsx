391435b2892f65efe7fad4f15b9f4b9a3373f5b8
AresHidalgo <foreveryalone88@gmail.com>
2025-07-09 23:22:13 -0500

    Configuraciones e implementaciones generales

--- src/layouts/publicLayout.jsx
+++ src/layouts/publicLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from '../components/ui/Header';
 import Footer from '../components/ui/Footer';
-import WhatsAppButton from "../components/share/whatsappButton";
+import WhatsAppButton from "../components/share/WhatsappButton";
 
 const publicLayout = () => {
 

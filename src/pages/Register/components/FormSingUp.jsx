883211d7939a35cba577f194e45e735f98fb8455
juanpablopos <juan_posada82211@elpoli.edu.co>
2025-07-11 01:04:23 -0500

    Avance en los metodos responsive

--- src/pages/Register/components/FormSingUp.jsx
+++ src/pages/Register/components/FormSingUp.jsx
@@ -1,64 +1,66 @@
 import Input from "../../../components/ui/Input";
-import Button from "../../../components/ui/Button";
 const FormSingUp = () => {
   return (
-    <div className="h-screen w-full  flex flex-col items-center justify-center bg-[#1E2A3A]">
-      <div className="bg-white h-auto w-96 p-8 rounded-xl shadow-md">
-        {/* Encabezado */}
-        <div className="text-base  mb-1">Bienvenido a JustHome </div>
-        <div className="text-3xl font-medium mb-6 text-black">Crear cuenta</div>
-        <form action="">
-          <Input
-            label="Nombre"
-            type="text"
-            placeholder="Nombre"
-            className="mb-2"
-            />
-            <Input
-            label="Apellido" 
-            type="text"
-            placeholder="Apellido"
-            className="mb-2" 
-            />
-            <Input
-            label="Correo Electrónico" 
-            type="email"
-            placeholder="Correo Electrónico"
-            className="mb-2" 
-            />
-            <Input
-            label="Telefono" 
-            type="tel"
-            placeholder="maxixo 10 digitos"
-            className="mb-2"     
-            />
-            <Input
-            label="Contraseña" 
-            type="password"
-            placeholder="Contraseña"
-            className="mb-2"     
-            />
-            <Input
-            label="Confirmar Contraseña" 
-            type="password"
-            placeholder="Confirmar Contraseña"
-            className="mb-2"     
-            />
+      <div className="h-auto w-auto">
+        <div className="flex flex-row gap-4 mb-4">
+          <div className="flex-1">
+            <form action="">
+              <Input
+                label="Nombre"
+                type="text"
+                placeholder="Nombre"
+                className="mb-2"
+                />
             </form>
-
-        {/* Botón de registro */}
-        <Button variant="primary" className="w-full  text-center py-3 rounded-lg  transition mb-4 mt-3">Registrarse</Button>
-        
-        {/* Enlace de registro */}
-        <div className="text-sm text-center text-gray-400">
-          ¿Ya tienes cuenta?{" "}
-          <span className="text-[#1E2A3A] font-medium cursor-pointer hover:underline">
-            Inicia Sesión
-          </span>
+          </div>
+          <div className="flex-1">
+            <form action="">
+              <Input
+                label="Apellido"
+                type="text"
+                placeholder="Apellido"
+                className="mb-2"
+                />
+            </form>
+          </div>
         </div>
-        
+
+        <div className="flex flex-row gap-4 mb-3 ">
+          <div className="flex-1">
+            <form action="">
+              <Input
+                label="Correo Electrónico"
+                type="email"
+                placeholder="Correo Electrónico"
+                className="mb-3"
+                />
+            </form>
+          </div>
+          <div className="flex-1">
+            <form action="">
+              <Input
+                label="Telefono"
+                type="tel"
+                placeholder="maxixo 10 digitos"
+                className="mb-2"
+                />
+            </form>
+          </div>
+        </div>
+
+        <Input
+          label="Contraseña"
+          type="password"
+          placeholder="Contraseña"
+          className="mb-2"
+          />
+        <Input
+          label="Confirmar Contraseña"
+          type="password"
+          placeholder="Confirmar Contraseña"
+          className="mb-2"
+          />
       </div>
-    </div>
   )
 }
 export default FormSingUp;
